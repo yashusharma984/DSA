f50b34b709b48f8602a8f26f2492135c39056f66
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-06-21 23:34:07 +0530

    commit -m

---
+++ Recursion programs/src/NumberExampleRecursion.java
@@ -0,0 +1,22 @@
+public class NumberExampleRecursion {
+    public static void main(String[] args) {
+        // write a function takes in a number and prints it
+        // print first 5 numbers: 1 2 3 4 5
+        print(1);
+    }
+    static void print(int n){
+        // base condition
+        if(n==5){
+            System.out.println(5);
+            return;
+        }
+        /*
+        recursive call
+         if you are calling a function again and again,
+          you can treat it as a seprate call in the stack
+         */
+
+        System.out.println(n);
+        print(n+1);
+    }
+}
