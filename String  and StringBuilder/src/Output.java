b1ac1754f8f58a23d434c8ca82169ae56f7c9afa
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-06-21 14:34:36 +0530

    Reapply "commit -m"
    This reverts commit e63acd006a88bc13a000907765a06a0252ca9b50.

---
+++ String  and StringBuilder/src/Output.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.lang.Integer;
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(56);
+        Integer num = new Integer(56);
+        System.out.println(num.toString());
+//        System.out.println(num);
+//        System.out.println("yash");
+//        System.out.println(new int[]{2,3,4,5});
+//
+//        String name = null;
+//        System.out.println(name);
+    }
+}
