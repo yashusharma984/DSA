a20145cfb5345dafae61d206e7931a9f20434454
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-06-26 14:27:53 +0530

    commit -m

---
+++ Bitwise Operator and Number System/src/oddEven.java
@@ -0,0 +1,10 @@
+// In this we are checking that number is Odd or Even...
+public class oddEven {
+    public static void main(String[] args) {
+       int n = 68;
+        System.out.println(isodd(n));
+    }
+    static boolean isodd(int n ){
+        return (n&1) == 1;
+    }
+}
