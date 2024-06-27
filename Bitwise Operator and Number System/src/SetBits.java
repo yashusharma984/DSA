2b8398ef4a03c7dfb1a0dbe7373c68482abf57dd
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-06-27 12:48:55 +0530

    commit -m

---
+++ Bitwise Operator and Number System/src/SetBits.java
@@ -0,0 +1,7 @@
+// In this we are finding the Binary NUmber in format of 0,1
+public class SetBits {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(Integer.toBinaryString(n));
+    }
+}
