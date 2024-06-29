23aa49aab41fddcb3de324bade50eb7659e6dca6
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-06-29 19:20:47 +0530

    commit -m

---
+++ RECURSION LVL 1/src/Reverse_num.java
@@ -0,0 +1,23 @@
+// Reverse A numbewr by using Recursion method
+public class Reverse_num {
+    public static void main(String[] args) {
+        rev1(1234);
+        System.out.println(rev);
+    }
+    static  int rev =0;
+    // method one to Reverse a number...
+    static void rev1(int n){
+        if(n==0){
+            return;
+        }
+        int rem = n%10;
+        rev =rev * 10 +rem;
+        rev1(n/10);
+    }
+
+    static void rev2(int n){
+        // sometimes you might
+        // need some additional variables in the arguments
+        // in that case, make another function
+    }
+}
