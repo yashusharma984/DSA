e8d963e4f6fdaf9f09ab83ba88a06cf263bd94a2
Yash Bhardwaj <132735857+yashusharma984@users.noreply.github.com>
2024-07-01 10:05:26 +0530

    commit -m

---
+++ RECURSION LVL 1/src/Count_num.java
@@ -0,0 +1,20 @@
+// In This Question we have to find the count
+// of zero in this term by using recursion method
+public class Count_num {
+    public static void main(String[] args) {
+        System.out.println(count(30201));
+    }
+    static int count(int n){
+        return helper(n,0);
+    }
+    private static int helper(int n , int c){
+     if(n==0){
+         return c;
+     }
+     int rem = n%10;
+     if(rem==0){
+         return helper(n/10,c+1);
+     }
+     return helper(n/10,c);
+    }
+}
